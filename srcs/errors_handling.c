ce8d4c0d23cb0e514910876578ef09081a22266d
Oleksandr Arseniy Ignatov <olarseni@c1r4s3.42madrid.com>
2024-11-25 21:32:31 +0100

    Finished the error handling file, updated some coments for stack_utils, and added the functions to initialize the stack. Pending start the main algorithm

--- srcs/errors_handling.c
+++ srcs/errors_handling.c
@@ -6,12 +6,14 @@
 /*   By: olarseni <olarseni@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 19:22:23 by olarseni          #+#    #+#             */
-/*   Updated: 2024/11/24 20:35:29 by olarseni         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:03:43 by olarseni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "error_handler.h"
+#define S_INT_MAX "2147483647"
+#define S_INT_MIN "-2147483648"
 
 /*
  * Function: error_message
@@ -26,7 +28,110 @@
 int	error_message(enum e_errors error)
 {
 	ft_putstr_fd("Error\n");
-	return (e);
+	return (error);
 }
 
+/*
+ * Function: check_valid_input
+ * ---------------------------
+ * This functions validate the input for each number it receive as string, if
+ * it is a valid number: Not NULL, not a void string, it's a number (positive
+ * or negative) or it is an integer, specified by the subject. Otherwise,
+ * it will close the program printing the "Error\n" message and exit with the 
+ * error number.
+ *
+ * num: String that contain a chain of numbers or a '-' sign followed by a chain
+ *      of numbers.
+ *
+ * return: true if it passed all the validations.
+ */
+int	check_valid_input(char *num)
+{
+	if (!num)
+		return (error_message(ERROR_NULL));
+	if (!*num)
+		return (error_message(ERROR_VOID));
+	if (!is_number(num))
+		return (error_message(ERROR_NOT_A_NUMBER));
+	if (!is_integer(num))
+		return (error_message(ERROR_NOT_AN_INTEGER));
+	return (OK);
+}
 
+/*
+ * Function: is_duplicated
+ * -----------------------
+ * This functions check if the number already exist in the created stack. If
+ * the stack doesn't exist, then it is the first element of it. If the number
+ * already exists in the stack, the function will exit with the error code for
+ * the duplicated numbers and print the error message.
+ * 
+ * num: The integer number.
+ * stack: The double linked list-based stack that store the numbers.
+ *
+ * return: return false if it passed the validation.
+ */
+int is_duplicated(int num, t_stack *stack)
+{
+	if (!stack)
+		return (OK);
+	while (stack && stack->value != num)
+		stack = stack->next;
+	if (stack && stack->value == num)
+		return (error_message(ERROR_DUP_NUM));
+	else
+		return (OK);
+}
+
+/*
+ * Function: is_number
+ * -------------------
+ * Validate if the received string is a valid positive number (all chars are 
+ * digits) or negative number ('-' followed by chain of digits).
+ *
+ * num: String that will be checked.
+ *
+ * returns: return true if it is a valid number or false if not.
+ */
+int	is_number(char *num)
+{
+	if (!num || !*num)
+		return (false);
+	if (*num == '-')
+		num++;
+	while (*num && ft_isdigit(*num))
+		num++;
+	if (!*num)
+		return (true);
+	else
+		return (false);
+}
+
+/*
+ * Function: is_integer
+ * --------------------
+ * Validade if the received string is a valid integer. It means that the number
+ * will fall in the space of the int type data range. The string must be a
+ * valid number (all digits or '-' followed by digits). Otherwise, the behavior
+ * is undefined.
+ *
+ * num: String that contain a number.
+ *
+ * return: Return true or false depending if it is an integer or not.
+ */
+int is_integer(char *num)
+{
+	int	len;
+
+	if (!num || !*num)
+		return (false);
+	len = ft_strlen(num);
+	if (len > 10 && ft_isdigit(*num))
+		return (false);
+	if (len > 11)
+		return (false);
+	if (len == 11 && *num == '-')
+		return (ft_strncmp(num, S_INT_MIN, 11) <= 0);
+	else
+		return (ft_strncmp(num, S_INT_MAX, 10) <= 0);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,97 @@
 /*   By: olarseni <olarseni@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:50:42 by olarseni          #+#    #+#             */
-/*   Updated: 2024/11/24 20:29:51 by olarseni         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:27:25 by olarseni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+ * Function: set_update_indexes
+ * ----------------------------
+ * This function go through the stack and check if the number is bigger or lower
+ * than the number setted in each node. If the number is bigger than the node's
+ * one, then grow the new index by 1. Otherwise, update the node's index by +1
+ *
+ * num: number to be checked.
+ * stack: the double linked list-based stack that will be checked.
+ *
+ * return: return the new index value for the new node added.
+ */
+int	set_update_indexes(int num, t_stack *stack)
+{
+	int	index;
+
+	if (!stack)
+		return (-1);
+	index = 0;
+	while (stack)
+	{
+		if (num > stack->value)
+			index++;
+		if (num < stack->value)
+			stack->index++;
+		stack = stack->next;
+	}
+	return (index);
+}
+
+/*
+ * Function: exit_and_free
+ * -----------------------
+ * Free the alocated memory in stack and exit the program with the error number
+ *
+ * stack: the double linked list-based stack that must to be freed.
+ * error: error number that will indicate where is the error.
+ * 
+ * return: No return
+ */
+void	exit_and_free(t_stack *stack, enum e_error error)
+{
+	stack_clear(stack);
+	exit(error);
+}
+
+/*
+ * Function: init_stack
+ * --------------------
+ * This function launch the checks to validade each number from the 'num_list'
+ * to validade the numbers, and if it pass it, the number will be added to the
+ * stack, and then, it will update the final indexes (final positions).
+ *
+ * num_list: array of strings of the numbers that are given as arguments to the
+ *           program, that are needed to validate and add to the stack.
+ *
+ * return: return the created stack or NULL if the num_list is NULL or exit
+ *         the program if there any error during the checks or memory reserve
+ *         with malloc.
+ */
+t_stack	*init_stack(char **num_list)
+{
+	enum e_errors	error;
+	t_stack			*stack;
+
+	if (!num_list)
+		return (NULL);
+	stack = NULL;
+	while (*num_list)
+	{
+		error = check_valid_input(*num_list);
+		if (error != OK);
+			exit_and_free(stack, error);
+		num = ft_atoi(*num_list);
+		error = is_duplicated(num, stack);
+		if (error != OK)
+			exit_and_free(stack, error);
+		stack = stack_add_last(&stack, stack_new(ft_atoi(num)));
+		stack_last(stack)->index = set_update_indexes(num, stack);
+		if (stack->index == -1)
+			exit_and_free(stack, ERROR_UPDATE_INDEX);
+		num_list++;
+	}
+	return (stack);
+}
 
 /*
  * Function: main
@@ -19,14 +105,19 @@
  * basic errors. Firstly, main function will initiate the double linked
  * list based stack and then will start the sorting algorithm. After sorting,
  * the stack will be cleared.
+ *
+ * argc: Number of arguments that receive the program.
+ * argv: Array of arguments that are passed to the program.
+ *
+ * return: Return OK from the enum e_error structure (has 0 as value).
  */
 int	main(int argc, char **argv)
 {
 	t_stack	*stack;
 
-	if (argc != 2)
+	if (argc < 2)
 		return (error_message(ERROR_N_ARGS));
-	stack = init_stack(argv[1]);
+	stack = init_stack(argv + 1);
 	if (!stack)
 		return (error_message(ERROR_INIT_STACK));
 	stack = push_swap(stack);
